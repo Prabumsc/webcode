7103006fb502637b976fdf8ad0be6d80bbd70abb
Prabumsc <119479769+Prabumsc@users.noreply.github.com>
2023-01-06 21:06:13 +0530

    Add files via upload
    webcode

---
+++ script.js
@@ -0,0 +1,34 @@
+var api='https://anapioficeandfire.com/api/characters'
+async function unsplash(){
+    console.log('data is loading');
+   
+    catch(err){
+        console.log(err);
+        console.log('error');
+    }
+}unsplash()
+         // try{
+    //     var data_fetch = await fetch(api)
+    //     var final_out= data_fetch.json()
+    //     var end_result=await final_out
+    //     console.log(end_result);
+    //     for(let i=0;i<end_result.length;i++){
+    //         //console.log(end_result[i].books)
+    //     }
+        
+       
+    //         }
+    
+    //var child_ele=document.createElement
+    //console.log(child_ele)
+    //child_ele.innerText=(end_result[i].books)
+    // document.querySelector('body').appendChild
+   
+ 
+
+
+
+
+
+
+
